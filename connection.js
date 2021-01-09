3a4f7d66cfa4c4a06b68875c8dd63d86a4a03c1c
Ian Wren <ian.wren81@gmail.com>
2021-01-09 09:24:52 -0800

    Added connection file to be used with function.js file by requiring it

---
+++ connection.js
@@ -0,0 +1,18 @@
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "employee-databaseDB"
+  });
+  connection.connect();
+  module.exports = connection;
+  
