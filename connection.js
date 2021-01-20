53ba0ac6f184aa58865d88bfb9a24f8bdeab3393
Ian Wren <ian.wren81@gmail.com>
2021-01-19 17:00:13 -0800

    Added a promisify to the connection to uses promises instead of async

--- connection.js
+++ connection.js
@@ -1,18 +1,24 @@
 const mysql = require("mysql");
+const util = require("util");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "187onacop",
-    database: "employee-databaseDB"
-  });
-  connection.connect();
-  module.exports = connection;
-  
+  host: "localhost",
+
+  // My port; if not 3306
+  port: 3306,
+
+  // My username
+  user: "root",
+
+  // My password
+  password: "187onacop",
+  database: "employee-databaseDB"
+});
+connection.connect();
+
+// Setting up connection.query to use promises instead of callbacks
+// This allows us to use the async/await syntax
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
+
