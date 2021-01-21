e19143fb1f8fabf0712eb68d458ec306cdcf4292
Ian Wren <ian.wren81@gmail.com>
2021-01-20 18:41:46 -0800

    Added a seeds file to the sql app

---
+++ seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO department (position, name) VALUES (1, "Sales");
+INSERT INTO role (position, role, salary, department_id) VALUES (1, "Salesperson", 150000, 1);
+INSERT INTO role (position, role, salary, department_id) VALUES (1, "Salesperson", 150000, 1);
