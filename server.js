f5ee435e5736722e1398db9c595a4d4115a44699
Ian Wren <ian.wren81@gmail.com>
2021-01-28 16:38:24 -0800

    Verified server calls

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const functions = require("./functions");
+const { prompt } = require("inquirer");
 // const connection  = require("./connection");
 
 
@@ -12,92 +13,101 @@ const functions = require("./functions");
 function runEmployeeData() {
   console.log("Welcome to employee tracker");
   inquirer.prompt({
-      name: "action",
-      type: "rawlist",
-      message: "What would you like to do?",
-      choices: [
-        "View All Employees",
-        "View All Employees by Department",
-        "View All Employees by Manager",
-        "Add Employee",
-        "Remove Employee",
-        "Update Employee Role",
-        "Update Employee Manager",
-        "View All Roles",
-        "Add Role",
-        "Remove Role",
-        "View All Departments",
-        "Add Department",
-        "Remove Department",
-        "View Total Budget"
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Employees by Department",
+      "View All Employees by Manager",
+      "Add Employee",
+      "Remove Employee",
+      "Update Employee Role",
+      "Update Employee Manager",
+      "View All Roles",
+      "Add Role",
+      "Remove Role",
+      "View All Departments",
+      "Add Department",
+      "Remove Department",
+      "View Total Budget"
 
 
-      ]
-    }).then(response => {
-      let action = response.action;
-      console.log(action);
-      console.log("Answer?", action);
-      switch (action) {
-        case "View All Employees":
-          functions.employeeSearch();
-          // viewEmployees();
-          break;
+    ]
+  }).then(response => {
+    let action = response.action;
+    console.log(action);
+    console.log("Answer?", action);
+    switch (action) {
+      case "View All Employees":
+        functions.employeeSearch();
+        // viewEmployees();
+        break;
 
-        case "View All Employees by Department":
-          functions.employeeDeptSearch();
-          break;
+      case "View All Employees by Department":
+        functions.employeeDeptSearch();
+        break;
 
-        case "View All Employees by Manager":
-          functions.employeeMangSearch();
-          break;
+      case "View All Employees by Manager":
+        functions.employeeMangSearch();
+        break;
 
-        case "Add Employee":
-          functions.addEmpSearch();
-          break;
+      case "Add Employee":
+        functions.addEmpSearch();
+        break;
 
-        case "Remove Employee":
-          functions.removeEmpSearch();
-          break;
+      case "Remove Employee":
+        functions.removeEmpSearch();
+        break;
 
-        case "Update Employee Role":
-          functions.updateEmpRoleSearch();
-          break;
+      case "Update Employee Role":
+        functions.updateEmpRoleSearch();
+        break;
 
-        case "Update Employee Manager":
-          functions.updateEmpMangSearch();
-          break;
+      case "Update Employee Manager":
+        functions.updateEmpMangSearch();
+        break;
 
-        case "View All Roles":
-          functions.rolesSearch();
-          break;
+      case "View All Roles":
+        functions.rolesSearch();
+        break;
 
-        case "Add Role":
-          functions.addRoleSearch();
-          break;
+      case "Add Role":
+        functions.addRoleSearch();
+        break;
 
-        case "Remove Role":
-          functions.removeRoleSearch();
-          break;
+      case "Remove Role":
+        functions.removeRoleSearch();
+        break;
 
-        case "View All Departments":
-          functions.deptSearch();
-          break;
+      case "View All Departments":
+        functions.deptSearch();
+        break;
 
-        case "Add Department":
-          functions.addDeptSearch();
-          break;
+      case "Add Department":
+        functions.addDeptSearch();
+        break;
 
-        case "Remove Department":
-          functions.removeDeptSearch();
-          break;
+      case "Remove Department":
+        functions.removeDeptSearch();
+        break;
 
-        case "View Total Budget":
-          functions.totalBudgetSearch();
-          break;
+      case "View Total Budget":
+        functions.totalBudgetSearch();
+        break;
 
+      default:
+        quit();
+        break;
 
-      }
-    });
+    }
+    // Exit the application
+
+  });
+  function quit() {
+    console.log("Goodbye!");
+    process.exit();
+  }
 }
 
 runEmployeeData();
